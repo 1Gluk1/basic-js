8738946db404dccb99e447cf8237ef0c390f289b
Anton Gorbachev <shieldmedia@yandex.ru>
2021-03-17 22:53:42 +0700

    feat: add dream-team function (3/10)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+
+  if (Array.isArray(members) != true) {
+    return false
+  }
+
+  let result = ""
+
+  for (i = 0; i < members.length; i++) {
+    if (typeof (members[i]) == "string") {
+      members[i] = members[i].split(" ").join("").toUpperCase()
+    }
+    else {
+      members[i] = 1
+    }
+  }
+
+  members = members.sort()
+
+  for (i = 0; i < members.length; i++) {
+    if (typeof (members[i]) == "string") {
+      result += members[i][0]
+    }
+  }
+  return result
+}
