882aaae261a9615156a80444bf8688a4a9a5a073
Anton Gorbachev <shieldmedia@yandex.ru>
2021-03-18 13:39:26 +0700

    feat: add extended repeater function (5/10)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = [];
+
+  if (typeof options["separator"] != 'string') {
+    options["separator"] = "+";
+  }
+
+  if (typeof options["additionSeparator"] != 'string') {
+    options["additionSeparator"] = "|";
+  }
+
+  if (typeof options["repeatTimes"] == 'undefined') {
+    options["repeatTimes"] = 1;
+  }
+
+  if (typeof options["additionRepeatTimes"] == 'undefined') {
+    options["additionRepeatTimes"] = 1;
+  }
+
+  if (options["repeatTimes"] >= 1) {
+    for (let i = 0; i < options["repeatTimes"]; i++) {
+      if (typeof options["addition"] !== 'undefined') {
+        if (options["additionRepeatTimes"] > 1) {
+          let additionSeparatorArra = [];
+          for (let i = 0; i < options["additionRepeatTimes"] - 1; i++) {
+            additionSeparatorArra[i] = options["additionSeparator"] + String(options["addition"]);
+          }
+          result.push(str + String(options["addition"]) + additionSeparatorArra.join(""));
+        } else {
+          result.push(str + options["addition"]);
+        }
+      } else {
+        result.push(str);
+      }
+    }
+  }
+  return result.join(options["separator"]);
 };
-  
