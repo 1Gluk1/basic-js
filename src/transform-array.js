f39e298b1e38d63ccad98cccfe4bc76a8005c6a6
Anton Gorbachev <shieldmedia@yandex.ru>
2021-03-30 02:10:32 +0700

    feat: add transform function (8/10)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next':
+        if (i + 1 < arr.length) result.push(arr[i + 1]);
+        break;
+      case '--discard-prev':
+        if (i != 0 && arr[i - 2] != '--discard-next') result.pop();
+        break;
+      case '--discard-next':
+        i++;
+        break;
+      case '--double-prev':
+        if (i != 0 && arr[i - 2] != '--discard-next') result.push(arr[i - 1]);
+        break;
+      default:
+        if (arr[i] != undefined) result.push(arr[i]);
+        break;
+    }
+  }
+  return result;
 };
